6e43ae7c43b0caece6b646a5920264174d04172f
Broodco <broodcoorens.maxime@gmail.com>
2023-07-18 10:40:17 +0200

    Completed navigation

---
+++ src/app/games/favorites/page.tsx
@@ -0,0 +1,7 @@
+export default function FavoriteGames() {
+  return (
+    <>
+      <div>Favorite games</div>
+    </>
+  )
+}
--- src/components/layout/NavBar.tsx
+++ src/components/layout/NavBar.tsx
@@ -5,11 +5,18 @@ import { Disclosure, Menu, Transition } from '@headlessui/react'
 import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/outline'
 import Link from "next/link";
 import Searchbar from "@/components/layout/SearchBar";
+import Image from "next/image";
+import {usePathname} from "next/navigation";
 
-const navigation: { name:string, href:string, current:boolean }[] = [
-  { name: 'Home', href: '/', current: true},
-  { name: 'Games', href: '/games', current: false},
-  { name: 'Favorites', href: '/favorites', current: false}
+const navigation: { name:string, href:string }[] = [
+  { name: 'Home', href: '/'},
+  { name: 'Games', href: '/games'},
+  { name: 'Favorites', href: '/games/favorites'}
+];
+
+const profileNavigation: { name:string, href:string }[] = [
+  { name: 'Profile', href: '/profile'},
+  { name: 'Settings', href: '/settings'},
 ];
 
 function classNames(...classes: string[]) {
@@ -17,31 +24,43 @@ function classNames(...classes: string[]) {
 }
 
 export default function Navbar() {
+  const pathname = usePathname();
+
   return (
     <Disclosure as="nav" className="bg-gray-800">
       {({ open }) => (
         <>
-          <div className="mx-auto px-2 sm:px-4 lg:px-8">
+          <div className="mx-auto px-2 sm:px-4 lg:pr-8">
             <div className="relative flex h-16 items-center justify-between">
               <div className="flex items-center px-2 lg:px-0">
-                <div className="flex-shrink-0 text-lg">
-                  E-G
-                </div>
+                <Link className="flex-shrink-0 text-lg flex flex-row" href="/">
+                  <Image
+                    src="/gameboy.svg"
+                    width={32}
+                    height={32}
+                    alt="Avatar"
+                  />
+                  <h2 className="hidden lg:block font-mono text-xl font-semibold pt-0.5 pl-2 text-gray-50">Everyone Games</h2>
+                </Link>
                 {/* Desktop Navbar */}
                 <div className="hidden lg:ml-6 lg:block">
                   <div className="flex space-x-4">
-                    {navigation.map(item => (
-                      <Link
-                        key={item.name}
-                        href={item.href}
-                        className={
-                          "rounded-md px-3 py-2 text-sm font-medium " +
-                          (item.current ? "bg-gray-800 text-white" : "text-gray-300 hover:bg-gray-700 hover:text-white")
-                        }
-                      >
-                        {item.name}
-                      </Link>
-                    ))}
+                    {navigation.map(item => {
+                      const isActive = pathname === item.href;
+
+                      return (
+                        <Link
+                          key={item.name}
+                          href={item.href}
+                          className={
+                            "rounded-md px-3 py-2 text-sm font-medium " +
+                            (isActive ? "bg-gray-800 text-white" : "text-gray-300 hover:bg-gray-700 hover:text-white")
+                          }
+                        >
+                          {item.name}
+                        </Link>
+                      )}
+                    )}
                   </div>
                 </div>
               </div>
@@ -67,10 +86,11 @@ export default function Navbar() {
                     <div>
                       <Menu.Button className="flex rounded-full bg-gray-800 text-sm text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
                         <span className="sr-only">Open user menu</span>
-                        <img
-                          className="h-8 w-8 rounded-full"
-                          src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-                          alt=""
+                        <Image
+                          src="/darth-maul-avatar.svg"
+                          width={32}
+                          height={32}
+                          alt="Avatar"
                         />
                       </Menu.Button>
                     </div>
@@ -84,32 +104,23 @@ export default function Navbar() {
                       leaveTo="transform opacity-0 scale-95"
                     >
                       <Menu.Items className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
-                        <Menu.Item>
-                          {({ active }) => (
-                            <a
-                              href="#"
-                              className={classNames(
-                                active ? 'bg-gray-100' : '',
-                                'block px-4 py-2 text-sm text-gray-700'
+                        {profileNavigation.map(item => {
+                          return (
+                            <Menu.Item key={item.name}>
+                              {({ active }) => (
+                                <Link
+                                  href={item.href}
+                                  className={classNames(
+                                    active ? 'bg-gray-100' : '',
+                                    'block px-4 py-2 text-sm text-gray-700'
+                                  )}
+                                >
+                                  {item.name}
+                                </Link>
                               )}
-                            >
-                              Your Profile
-                            </a>
+                            </Menu.Item>
                           )}
-                        </Menu.Item>
-                        <Menu.Item>
-                          {({ active }) => (
-                            <a
-                              href="#"
-                              className={classNames(
-                                active ? 'bg-gray-100' : '',
-                                'block px-4 py-2 text-sm text-gray-700'
-                              )}
-                            >
-                              Settings
-                            </a>
-                          )}
-                        </Menu.Item>
+                        )}
                         <Menu.Item>
                           {({ active }) => (
                             <a
@@ -134,43 +145,32 @@ export default function Navbar() {
           {/* Mobile panel */}
           <Disclosure.Panel className="lg:hidden">
             <div className="space-y-1 px-2 pb-3 pt-2">
-              {/* Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" */}
-              <Disclosure.Button
-                as={Link}
-                href="/"
-                className="block rounded-md bg-gray-900 px-3 py-2 text-base font-medium text-white"
-              >
-                Home
-              </Disclosure.Button>
-              <Disclosure.Button
-                as={Link}
-                href="/games"
-                className="block rounded-md px-3 py-2 text-base font-medium text-gray-300 hover:bg-gray-700 hover:text-white"
-              >
-                Popular
-              </Disclosure.Button>
-              <Disclosure.Button
-                as={Link}
-                href="/games"
-                className="block rounded-md px-3 py-2 text-base font-medium text-gray-300 hover:bg-gray-700 hover:text-white"
-              >
-                All games
-              </Disclosure.Button>
-              <Disclosure.Button
-                as={Link}
-                href="/games"
-                className="block rounded-md px-3 py-2 text-base font-medium text-gray-300 hover:bg-gray-700 hover:text-white"
-              >
-                Favorites
-              </Disclosure.Button>
+              {navigation.map(item => {
+                const isActive = pathname === item.href;
+
+                return (
+                  <Disclosure.Button
+                    as={Link}
+                    key={item.name}
+                    href={item.href}
+                    className={
+                      "block rounded-md px-3 py-2 text-base font-medium " +
+                      (isActive ? "bg-gray-900 text-white" : "text-gray-300 hover:bg-gray-700 hover:text-white")
+                    }
+                  >
+                    {item.name}
+                  </Disclosure.Button>
+                )}
+              )}
             </div>
             <div className="border-t border-gray-700 pb-3 pt-4">
               <div className="flex items-center px-5">
                 <div className="flex-shrink-0">
-                  <img
-                    className="h-10 w-10 rounded-full"
-                    src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-                    alt="avatar"
+                  <Image
+                    src="/darth-maul-avatar.svg"
+                    width={44}
+                    height={44}
+                    alt="Avatar"
                   />
                 </div>
                 <div className="ml-3">
@@ -184,20 +184,23 @@ export default function Navbar() {
                 </button>
               </div>
               <div className="mt-3 space-y-1 px-2">
-                <Disclosure.Button
-                  as="a"
-                  href="#"
-                  className="block rounded-md px-3 py-2 text-base font-medium text-gray-400 hover:bg-gray-700 hover:text-white"
-                >
-                  Your Profile
-                </Disclosure.Button>
-                <Disclosure.Button
-                  as="a"
-                  href="#"
-                  className="block rounded-md px-3 py-2 text-base font-medium text-gray-400 hover:bg-gray-700 hover:text-white"
-                >
-                  Settings
-                </Disclosure.Button>
+                {profileNavigation.map(item => {
+                  const isActive = pathname === item.href;
+
+                  return (
+                    <Disclosure.Button
+                      as={Link}
+                      key={item.name}
+                      href={item.href}
+                      className={
+                        "block rounded-md px-3 py-2 text-base font-medium " +
+                        (isActive ? "bg-gray-900 text-white" : "text-gray-400 hover:bg-gray-700 hover:text-white")
+                      }
+                    >
+                      {item.name}
+                    </Disclosure.Button>
+                  )}
+                )}
                 <Disclosure.Button
                   as="a"
                   href="#"
