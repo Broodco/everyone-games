a7a56eef7599e06af2bd197f9704e96814c09c1e
Broodco <broodcoorens.maxime@gmail.com>
2023-07-28 02:12:34 +0200

    Started single game page

--- src/app/games/[id]/page.tsx
+++ src/app/games/[id]/page.tsx
@@ -1,5 +1,6 @@
 import { Comfortaa } from "next/font/google"
-import { Game, GameDetails } from "@/types/types_Games"
+import { GameDetails } from "@/types/types_Games"
+import { ChevronRightIcon } from '@heroicons/react/20/solid'
 
 const comfortaa = Comfortaa({ subsets: ["latin"] })
 
@@ -22,9 +23,20 @@ export default async function Game({ params }: { params: { id: string } }) {
 
   return (
     <>
-      <div>Game : {gameDetails.name}</div>
+      <div className="lg:relative flex flex-col lg:flex-none">
+        <h1 className="lg:absolute text-4xl font-bold tracking-tight text-white z-40 bg-gray-900 p-4 rounded-md lg:mt-28 max-w-md">{gameDetails.name}</h1>
+        <img src={gameDetails.background_image} alt="Game background" className="w-full max-h-96 lg:w-full lg:max-w-4xl lg:h-96 object-cover lg:ml-28 rounded-md"/>
+      </div>
 
-      <div>{JSON.stringify(gameDetails)}</div>
+      <div className="flex flex-col lg:flex-row mt-8 mx-6 justify-around">
+        <div className="basis-1/2 px-8 py-4">
+          <h2 className="text-white tracking-tight text-xl font-semibold mb-4">About the game :</h2>
+          <p dangerouslySetInnerHTML={ { __html: gameDetails.description } } />
+        </div>
+        <div className="basis-1/2">
+
+        </div>
+      </div>
     </>
   )
 }
